3f98d801584b2fe82324826a2fcf024167201a36
Alex Melnikov <melnikov@gmail.com>
2011-11-04 02:42:31 +0300

    Initial Commit

---
+++ Quiet Panicker/Quiet_PanickerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Quiet_PanickerAppDelegate.h
+//  Quiet Panicker
+//
+//  Created by melnikov on 11/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiet_PanickerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
